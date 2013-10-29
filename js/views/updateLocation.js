aba1bc3417c8bc7c6d0a5307f97153a4fac8d962
b108.frolovo <b108@yandex.ru>
2013-10-29 12:30:34 +0400

    interval location update

--- js/views/updateLocation.js
+++ js/views/updateLocation.js
@@ -2,9 +2,11 @@ define(['Backbone', 'jQuery', 'EventBroker'], function(Backbone, $, EventBroker)
     return Backbone.View.extend({
         initialize: function() {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    EventBroker.trigger('map:setCenter', {lon: position.coords.longitude, lat: position.coords.latitude, z: 18});
-                });
+                setInterval(function() {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        EventBroker.trigger('map:setCenter', {lon: position.coords.longitude, lat: position.coords.latitude, z: 18});
+                    });
+                }, 10000);
             }
         }
     });
