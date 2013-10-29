e5996a6805197b16fccace3cee088955a6e779ff
b108.frolovo <b108@yandex.ru>
2013-10-29 15:34:30 +0400

    fix

--- js/functions/distanceMeters.js
+++ js/functions/distanceMeters.js
@@ -1,6 +1,6 @@
 define(['Leaflet'], function(L) {
     return function(latlon1, latlon2) {
-        var latlon = new L.LatLng([latlon1.lat, latlon1.lon]);
+        var latlon = new L.LatLng(latlon1.lat, latlon1.lon);
 
         return latlon.distanceTo([latlon2.lat, latlon2.lon]);
     }
