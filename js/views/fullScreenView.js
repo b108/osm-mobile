a27a9f6967149a4c951809593dce5b8d59ca8b6a
b108.frolovo <b108@yandex.ru>
2013-10-29 20:47:46 +0400

    for debug

--- js/views/fullScreenView.js
+++ js/views/fullScreenView.js
@@ -12,7 +12,11 @@ define(['Backbone', 'jQuery', 'detect/fullScreen'], function(Backbone, $, detect
         },
         goFullScreenClickHandler: function(event) {
             detect.fullScreen( $('html')[0] );
-            $(window).resize();
+
+            setInterval(function() {
+                $(window).resize();
+            }, 1000);
+
             return false;
         }
     });
